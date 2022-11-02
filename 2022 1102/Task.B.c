c57e7e66280e03ab25bb1883fecedc700752be94
MastuKen <matsumotokenta532@gmail.com>
2022-11-02 16:27:24 +0900

    2022 1102 アルゴリズム授業課題　下記の1~6全部完了
    1. 20221102ディレクトリを作成
    2. TaskA.cファイルを作成する
    3. TaskA.cに1から10まで順に出力するプログラムを書く
    3. TaskB.cファイルを作成する
    4. TaskB.cに乱数を10個格納した配列を作成し、出力するプログラムを書く
    5. TaskC.cファイルを作成する
    6. TaskC.cに1から100の整数で、3の倍数はfizz・5の倍数はbuzz・3と5の倍数はfizz buzzを出
    力するプログラムを書く

---
+++ 2022 1102/Task.B.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+  srand(time(NULL));
+  int R=0;
+   int RAND[10] = {};   //配列の宣言
+    for(int i = 0; i <=9; i++) 
+    {
+            R = rand() % 100+ 1; /* 1 〜 100 のランダムな数値 */
+            RAND[i]= R;
+            R=0;
+    }
+            //printf("%d,", RAND[i]);
+  /* 出力 */
+      for(int i = 0; i <=9; i++) 
+    {
+    printf("%d,", RAND[i]);
+    }
+    return 0;
+}
