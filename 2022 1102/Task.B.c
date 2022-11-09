f5b9798a790f1dfee5be62c6835f0af11c3a2373
MastuKen <matsumotokenta532@gmail.com>
2022-11-09 15:08:15 +0900

    TaskA.cにif文で書いたので,TascB.cにwhile文で書いた

--- 2022 1102/Task.B.c
+++ 2022 1102/Task.B.c
@@ -1,23 +1,12 @@
 #include<stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 int main()
 {
-  srand(time(NULL));
-  int R=0;
-   int RAND[10] = {};   //配列の宣言
-    for(int i = 0; i <=9; i++) 
-    {
-            R = rand() % 100+ 1; /* 1 〜 100 のランダムな数値 */
-            RAND[i]= R;
-            R=0;
-    }
-            //printf("%d,", RAND[i]);
-  /* 出力 */
-      for(int i = 0; i <=9; i++) 
-    {
-    printf("%d,", RAND[i]);
-    }
-    return 0;
+int i=1;
+while(i<=10)
+{
+    printf("%d\n", i);
+    i++;
+}
+return 0;
 }
