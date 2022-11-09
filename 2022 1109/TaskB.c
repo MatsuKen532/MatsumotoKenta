98ec9dd9bd4f591b09132358cddb301d7aa8451f
MastuKen <matsumotokenta532@gmail.com>
2022-11-09 16:22:23 +0900

    TaskB.cに乱数が100個入った配列を作成し、その配列をバブルソートするプログラムを書いた

---
+++ 2022 1109/TaskB.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void RANDOM(int [100]);
+int main()
+{
+  srand(time(NULL));
+  //int R=0;
+   int RAND[100] = {};   //配列の宣言
+   int Rand[100] = {};   //配列の宣言
+   RANDOM(RAND);
+   RANDOM(Rand);
+int i,j;
+int tmp;
+for(i=0;i<100;i++)
+{
+    for(j=i+1;j<100;j++)
+    {
+        if(RAND[i]>RAND[j])
+        {
+            tmp=RAND[i];
+            RAND[i]=RAND[j];
+            RAND[j]=tmp;
+        }
+    }
+}
+for(i=0;i<100;i++)
+{
+    for(j=i+1;j<100;j++)
+    {
+        if(Rand[i]<Rand[j])
+        {
+            tmp=Rand[i];
+            Rand[i]=Rand[j];
+            Rand[j]=tmp;
+        }
+    }
+}
+            //printf("%d,", RAND[i]);
+  /* 出力 */
+      for(int i = 0; i <=100; i++) 
+    {
+        printf("%d \n,", RAND[i]);
+    }
+      for(int i = 0; i <=100; i++) 
+    {
+    printf("%d \n,", Rand[i]);
+    }
+    return 0;
+}
+void RANDOM(int temp[100]){
+    for(int i=0;i<100;++i){
+        int R = rand() % 1000+ 1; /* 1 〜 100 のランダムな数値 */
+         temp[i]= R;
+        R=0; 
+    }
+}
