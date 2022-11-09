00547177f5e00e9c49ece239ac067d158a9886d7
MastuKen <matsumotokenta532@gmail.com>
2022-11-09 15:09:14 +0900

    TaskC.cに乱数を10個格納した配列を作成し、出力するプログラムを書いた

--- 2022 1102/TaskC.c
+++ 2022 1102/TaskC.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
-int main(){
-    for(int i=1;i<=100;i++){
-        if(i%3==0&&i%5==0)
-        {
-            printf("fizz buzz\n");
-        }
-        else if(i%3==0)
-        {
-            printf("fizz\n");
-        }
-        else if(i%5==0)
-        {
-            printf("buzz\n");
-        }
+#include <stdlib.h>
+#include <time.h>
 
+int main()
+{
+  srand(time(NULL));
+  int R=0;
+   int RAND[10] = {};   //配列の宣言
+    for(int i = 0; i <=9; i++) 
+    {
+            R = rand() % 100+ 1; /* 1 〜 100 のランダムな数値 */
+            RAND[i]= R;
+            R=0;
     }
-
-
-
+            //printf("%d,", RAND[i]);
+  /* 出力 */
+      for(int i = 0; i <=9; i++) 
+    {
+    printf("%d,", RAND[i]);
+    }
     return 0;
 }
